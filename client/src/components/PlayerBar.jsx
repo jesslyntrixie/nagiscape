bcdcfcebfdb4ac353df640d8c576d1ba52613ed8
Jesslyn <jesslynedvilie@gmail.com>
2025-09-08 14:54:37 +0700

    benerin layout lebar

--- client/src/components/PlayerBar.jsx
+++ client/src/components/PlayerBar.jsx
@@ -2,7 +2,7 @@
 
 function PlayerBar({ currentTrack, isAppPlaying, isMusicPlaying, onSaveMixClick, onPlayPauseClick, currentMixName }){
     return(
-        <div className="player-bar glass-panel flex-between">
+        <div className="player-bar glass-effect flex-between">
             <div className="now-playing">
                 <div className="now-playing-icon flex-center">
                     <i className="ri-headphone-line"></i>
@@ -15,16 +15,18 @@ function PlayerBar({ currentTrack, isAppPlaying, isMusicPlaying, onSaveMixClick,
                 </div>
             </div>
             
-            <div className="player-controls">
-                <button className="play-pause-btn flex-center" onClick={onPlayPauseClick}>
-                    {isAppPlaying ? <i className="ri-pause-fill"></i> : <i className="ri-play-fill"></i>}
-                </button>
-            </div>
-            <div className="save-btn-container"> 
-                <button className="btn btn--filled" onClick={onSaveMixClick}>
-                    <i className="ri-add-line"></i>
-                    <span>Save Mix</span>
-                </button>
+            <div className="player-bar-right">
+                <div className="player-controls">
+                    <button className="play-pause-btn flex-center" onClick={onPlayPauseClick}>
+                        {isAppPlaying ? <i className="ri-pause-fill"></i> : <i className="ri-play-fill"></i>}
+                    </button>
+                </div>
+                <div className="save-btn-container"> 
+                    <button className="btn btn--filled" onClick={onSaveMixClick}>
+                        <i className="ri-add-line"></i>
+                        <span>Save Mix</span>
+                    </button>
+                </div>
             </div>
         </div>  
     );
