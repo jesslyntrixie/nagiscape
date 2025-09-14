748ebc048452f589a4b401c833ff47da24b940b3
Jesslyn <jesslynedvilie@gmail.com>
2025-09-14 18:30:05 +0700

    icon and credits

--- client/src/components/CreditsPage.jsx
+++ client/src/components/CreditsPage.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function CreditsPage({ musicTracks = [], ambienceSounds = [] }) {
+  const visualCredits = {
+    icons: [
+      {
+        html: `<a href="https://www.flaticon.com/free-icons/moon" title="moon icons">Moon icons created by Freepik - Flaticon</a>`
+      }
+    ],
+    video: [
+      {
+        name: 'Cozy Room Window at Night',
+        sourceUrl: 'https://pixabay.com/videos/lofi-cat-night-window-rain-cute-246243/' 
+      }
+    ]
+  };
+
   return (
     <div className="credits-page-container">
       <header className="settings-header">
@@ -12,10 +26,9 @@ function CreditsPage({ musicTracks = [], ambienceSounds = [] }) {
         <h2>Credits & Attribution</h2>
       </header>
       
-      {/* Gunakan grid untuk layout utama */}
       <div className="credits-grid">
 
-        {/* Kolom untuk Musik */}
+        
         <div className="credits-column">
           <div className="credits-header">
             <i className="ri-music-2-line"></i>
@@ -49,7 +62,43 @@ function CreditsPage({ musicTracks = [], ambienceSounds = [] }) {
             ))}
           </ul>
         </div>
+      </div>
+      <div className="credits-grid visual-credits">
+        {/* Kolom untuk Ikon */}
+        <div className="credits-column">
+          <div className="credits-header">
+            <i className="ri-pencil-ruler-2-line"></i>
+            <h3>Icons & Design</h3>
+          </div>
+          <ul className="credits-list">
+            {visualCredits.icons.map((icon, index) => (
+              <li className="credit-item" key={`icon-${index}`}>
+                <div 
+                  className="credit-html" 
+                  dangerouslySetInnerHTML={{ __html: icon.html }} 
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
 
+        {/* Kolom untuk Video */}
+        <div className="credits-column">
+          <div className="credits-header">
+            <i className="ri-film-line"></i>
+            <h3>Background Video</h3>
+          </div>
+          <ul className="credits-list">
+            {visualCredits.video.map((vid, index) => (
+              <li className="credit-item" key={`video-${index}`}>
+                <span className="credit-title">{vid.name}</span>
+                <a href={vid.sourceUrl} className="credit-source" target="_blank" rel="noopener noreferrer">
+                  Source<i className="ri-external-link-line"></i>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
