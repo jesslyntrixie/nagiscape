87e429a9d923d7d60118487d494f591c58f9f036
Jesslyn <jesslynedvilie@gmail.com>
2025-09-22 13:50:49 +0700

    edit vercel.json and index.js, removing express and letting vercel access it as static files

--- server/api/index.js
+++ server/api/index.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(cors()); 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use((req, res, next) => {
   console.log(`Request Diterima: ${req.method} ${req.originalUrl}`);
