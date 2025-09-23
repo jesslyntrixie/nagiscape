9cb4ffe4853fec94a0cf29aa38d472740e2fd6ab
Jesslyn <jesslynedvilie@gmail.com>
2025-09-23 10:45:45 +0700

    edit cors

--- server/api/index.js
+++ server/api/index.js
@@ -16,7 +16,13 @@ const path = require('path');
 const app = express();
 
 // Middleware
-app.use(cors()); 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL
+};
+
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, '..', 'public')));
 
